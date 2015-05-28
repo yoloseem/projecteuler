4c8b11cda105f5a3e18cc53dd70f0d595899dae5
Hyunjun Kim <yoloseem@gmail.com>
2015-05-28 15:48:33 +0900

    Use boolean for Eratosthenes' sieve

--- 0003/0003.rs
+++ 0003/0003.rs
@@ -1,15 +1,15 @@
 fn main() {
     let num: i64 = 600851475143;
     let factor_limit = (num as f64).sqrt() as i32;
-    let mut sieve: Vec<i32> = vec![0; (factor_limit + 1) as usize];
+    let mut sieve: Vec<bool> = vec![true; (factor_limit + 1) as usize];
     let mut largest_prime_factor = 0;
     for factor in (2..factor_limit) {
-        if sieve[factor as usize] == 0 {
+        if sieve[factor as usize] == true {
             if num % (factor as i64) == 0 {
                 largest_prime_factor = factor;
             }
             for i in (2..(factor_limit / factor + 1) as i32) {
-                sieve[(factor * i) as usize] = 1;
+                sieve[(factor * i) as usize] = false;
             }
         }
     }
